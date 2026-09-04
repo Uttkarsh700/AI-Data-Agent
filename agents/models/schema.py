b672057e3d970e23e5c075adfe361de2086092e3
Uttkarsh Joshi <uttkarshjoshi7417@gmail.com>
2026-09-04 13:57:07 +0530

    Extend agent schema and utilities

--- agents/models/schema.py
+++ agents/models/schema.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel, Field
-from typing import Annotated
+from typing import Annotated, Literal
+from langgraph.graph.message import add
 
 class AgentSchema(BaseModel):
     message : Annotated[list,add] = Field(..., description="List of messages to be processed by the agent.")
+    curated_ques : str = Field(..., description="List of curated user questions.")
+    prompt_query_context : str = Field(..., description="A detailed prompt with SQL DB context that will help agent to generate SQL query.")
+    is_safe : Literal["Yes", "No"] = Field(..., description="Whether the query is safe or not.")
+    generated_sql_query : str = Field(..., description="Generated SQL query.")
+    sql_query_execution_result : str = Field(..., description="SQL query execution result.")
+    final_answer : str = Field(..., description="Final answer.")
